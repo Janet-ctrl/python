c0dd0cf458343966a45fca84f63a82167ac601bd
Janet-ctrl <janet.eras@outlook.com>
2020-06-09 11:44:59 +0200

    update

--- Chapter2/try7529.py
+++ Chapter2/try7529.py
@@ -1,4 +1,4 @@
 fav_num = 26
 message = "Hi! My favorite number is "
 
-print(f'{message} {fav_num}')
+print(f"{message} {fav_num}!")
---
+++ Chapter2/try76210.py
@@ -0,0 +1,7 @@
+# Printing calculations with the solution value of 8.
+
+print(9-1)
+print(4+4)
+print(5+3)
+print(10-2)
+
